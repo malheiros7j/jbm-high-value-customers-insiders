0f6927af2571cd91d86fd889fef928f826d1f589
Jordan Malheiros <malheiros7j@gmail.com>
2022-09-28 17:57:07 -0400

    bash to run at aws vps

---
+++ run_model_production.sh
@@ -0,0 +1,12 @@
+# variable
+data=$(date +'%Y-%m-%dT%H:%M:%S')
+
+
+# path (root of project)
+path='/home/ubuntu/Fidelity-Program'
+
+# path where papermill is installed on env (find: 'which papermill' on project root folder)
+path_to_envs='/home/ubuntu/anaconda3/envs/pa005_clustering_aws/bin'
+
+
+$path_to_envs/papermill $path/src/models/9.0-nmv-deploy.ipynb $path/reports/9.0-nmv-deploy-$data.ipynb
